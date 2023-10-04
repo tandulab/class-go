d2cb4f9a91bb9946b1cc0349fe9ac8b714a233b6
Sara <sarabertaina@hotmail.it>
2023-10-04 10:47:07 +0200

    fix

--- assets/Logo14579.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,b as s,e}from"../index14579.js";const t="/assets/logo14579.png",c={},n={src:t,alt:"Logo"};function r(_,a){return s(),e("img",n)}const m=o(c,[["render",r]]);export{m as L};
