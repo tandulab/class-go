efd2214ceadefd7ade095bfae24f3eb548186f91
REDANDBLUE\sarab <sarabertaina@hotmail.it>
2024-04-16 11:18:03 +0200

    new build

---
+++ assets/Logo56232.js
@@ -0,0 +1,1 @@
+import{_ as o,h as s,i as t}from"../index56232.js";const c="/assets/logo56232.png",e={},n={src:c,alt:"Logo"};function r(_,a){return s(),t("img",n)}const m=o(e,[["render",r]]);export{m as L};
