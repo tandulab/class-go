538dc485d57e5bd197e0e734fc16e72782e40e90
Sara <sarabertaina@hotmail.it>
2024-04-19 11:34:54 +0200

    new build

--- assets/Logo65547.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,h as s,i as t}from"../index65547.js";const c="/assets/logo65547.png",e={},n={src:c,alt:"Logo"};function r(_,a){return s(),t("img",n)}const m=o(e,[["render",r]]);export{m as L};
