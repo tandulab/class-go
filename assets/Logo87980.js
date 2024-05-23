78c45e1b1c1974cc1edd1ba88f0281bbc5ef07cd
GiacomoTandu <g.damosso@tandu.it>
2024-05-23 17:37:39 +0200

    build

--- assets/Logo87980.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,h as s,i as t}from"../index87980.js";const c="/assets/logo87980.png",e={},n={src:c,alt:"Logo"};function r(_,a){return s(),t("img",n)}const m=o(e,[["render",r]]);export{m as L};
