ca67c5368a12973d439aca1aa19cc8a568601b88
GiacomoTandu <g.damosso@tandu.it>
2024-05-24 15:34:09 +0200

    build

---
+++ assets/Logo87373.js
@@ -0,0 +1,1 @@
+import{_ as o,h as s,i as t}from"../index87373.js";const c="/assets/logo87373.png",e={},n={src:c,alt:"Logo"};function r(_,a){return s(),t("img",n)}const m=o(e,[["render",r]]);export{m as L};
