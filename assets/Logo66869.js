14610d8da29d5ec664f53457298d70764f629aaf
GiacomoDamosso <g.damosso@tandu.it>
2024-10-07 14:05:11 +0200

    build

--- assets/Logo66869.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,h as s,i as t}from"../index66869.js";const c="/assets/logo66869.png",e={},n={src:c,alt:"Logo"};function r(_,a){return s(),t("img",n)}const m=o(e,[["render",r]]);export{m as L};
