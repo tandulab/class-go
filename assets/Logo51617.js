5d7dccb3f146bae38056ab7faaeb93b5275f7e6c
GiacomoDamosso <g.damosso@tandu.it>
2025-12-09 10:46:35 +0100

    build

---
+++ assets/Logo51617.js
@@ -0,0 +1,1 @@
+import{_ as o,h as s,i as t}from"../index51617.js";const c="/assets/logo51617.png",e={},n={src:c,alt:"Logo"};function r(_,a){return s(),t("img",n)}const m=o(e,[["render",r]]);export{m as L};
